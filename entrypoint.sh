7830000a71978a9e234865b83bfea1ba9973597d
PJ <pj@notanemail.com>
2021-04-25 10:25:01 -0400

    Updated entrypoint.sh to be able to parse the postgres url provided by Heroku

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,14 @@ if [ -n "$DB_PASSWORD_FILE" ]; then
   export DB_PASSWORD=$(cat $DB_PASSWORD_FILE)
 fi
 
+# This one to accomodate Heroku Postgres (or to provide a single secret value
+# vs multiple)
+if [ -n "$DATABASE_URL" ]; then
+  export DB_DRIVER=$(echo "$DATABASE_URL" | cut -d ':' -f 1)
+  export DB_USER=$(echo "$DATABASE_URL" | cut -d '/' -f 3 | cut -d ':' -f 1)
+  export DB_PASSWORD=$(echo "$DATABASE_URL" | cut -d ':' -f 3 | cut -d '@' -f 1)
+  export DB_HOST=$(echo "$DATABASE_URL" | cut -d '@' -f 2 | cut -d '/' -f 1)
+  export DB_NAME=$(echo "$DATABASE_URL" | cut -d '/' -f 4)
+fi
+
 /accounting/run.sh
